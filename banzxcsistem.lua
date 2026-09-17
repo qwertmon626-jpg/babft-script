5de5dc9a3d1f65eb7e59fff13cf0b1cd672e4703
qwertmon626-jpg <qwertmon626@gmail.com>
2026-09-17 16:58:18 +0300

    Update banzxcsistem.lua

--- banzxcsistem.lua
+++ banzxcsistem.lua
@@ -12,7 +12,7 @@ local UserId = LocalPlayer.UserId
 -- Белый список и лимиты заходов в день
 local Whitelist = {
     [7982855852] = 999, -- tubers0268
-    [1395207311] = 3,   -- ilysha23112000
+    [] = 3,   -- ilysha23112000
     [5635347980] = 3,   -- Papirus333564
     [3841899130] = 5,   -- Dvanseler
 }
